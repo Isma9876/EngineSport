708944118d4f60d792f7970ee941bc1131ea5df1
Lytzy Xitumul <lxitumull2@miumg.edu.gt>
2026-09-14 15:04:01 -0600

    fix: usar producto.id en vez de producto.id_producto en pedido.controller.js

--- app/controllers/pedido.controller.js
+++ app/controllers/pedido.controller.js
@@ -47,14 +47,14 @@ exports.crearPedido = async (req, res) => {
       subtotal += subtotalItem;
 
       detalles.push({
-        id_producto: producto.id_producto,
+        id_producto: producto.id,
         cantidad: item.cantidad,
         precio_unitario: precioUnitario,
         subtotal: subtotalItem,
       });
 
       movimientos.push({
-        id_producto: producto.id_producto,
+        id_producto: producto.id,
         cantidad: item.cantidad,
       });
 
